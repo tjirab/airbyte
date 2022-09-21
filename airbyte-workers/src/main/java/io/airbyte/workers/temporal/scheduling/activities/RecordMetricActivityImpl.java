69b29350eefd682b8d6f99e1bc24eb453d3aa692
jdpgrailsdev <jpearlin1@gmail.com>
2022-09-21 16:07:36 -0400

    Fix NPE

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/activities/RecordMetricActivityImpl.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/activities/RecordMetricActivityImpl.java
@@ -45,7 +45,7 @@ public class RecordMetricActivityImpl implements RecordMetricActivity {
   @Trace(operationName="activity")
   @Override
   public void recordWorkflowCountMetric(final RecordMetricInput metricInput) {
-    TraceUtils.addTagsToTrace(Map.of("connection-id", metricInput.getConnectionUpdaterInput().getConnectionId(), "job-id", metricInput.getConnectionUpdaterInput().getJobId()));
+    TraceUtils.addTagsToTrace(Map.of("connection-id", metricInput.getConnectionUpdaterInput().getConnectionId()));
     final List<MetricAttribute> baseMetricAttributes = generateMetricAttributes(metricInput.getConnectionUpdaterInput());
     if (metricInput.getMetricAttributes() != null) {
       baseMetricAttributes.addAll(Stream.of(metricInput.getMetricAttributes()).collect(Collectors.toList()));
